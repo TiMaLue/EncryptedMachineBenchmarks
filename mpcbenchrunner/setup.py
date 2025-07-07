b554054b9724644c25b78d85289a2c94a82960a6
Tim Lüdeke <tim.luedeke@uni-wuppertal.de>
2025-07-07 15:11:47 +0200

    Revert "add tomlkit dependency for scheduler files"
    This reverts commit 1a98aabca2001c490155819f22eff33796714fdf.

--- mpcbenchrunner/setup.py
+++ mpcbenchrunner/setup.py
@@ -1,9 +1,14 @@
 from distutils.core import setup
 
-setup(
-    name="mpcbenchrunner",
-    version="1.0",
-    author_email="amin.faez.inbox@gmail.com",
-    packages=["mpcbenchrunner"],
-    install_requires=["commonsnakes", "pyyaml", "attrs", "cattrs", "docker", "tomlkit"],
+setup(name='mpcbenchrunner',
+    version='1.0',
+    author_email='amin.faez.inbox@gmail.com',
+    packages=['mpcbenchrunner'],    
+    install_requires=[
+        'commonsnakes',
+        'pyyaml',
+        'attrs',
+        'cattrs',
+        'docker'
+    ],
 )
